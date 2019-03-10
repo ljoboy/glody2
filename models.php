6804bbcddc56703b4484de7209616f4f967df6cb
Dark Angel <jonathanyombo@gmail.com>
2019-03-10 09:24:18 +0200

    function dbConnect

--- models.php
+++ models.php
@@ -1,7 +1,11 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ITOT
- * Date: 10/03/2019
- * Time: 09:11 AM
- */
+if (!function_exists('dbConnect')){
+    function dbConnect(){
+        try{
+            $db = new PDO('mysql:dbname=glody2;host=localhost','root', '');
+        }catch (PDOException $e){
+            die('Erreur du au : '.$e->getMessage());
+        }
+        return $db;
+    }
+}
