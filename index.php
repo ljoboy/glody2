9254efea2fe6068a1085732fdb7bc15f7fac4170
Dark Angel <jonathanyombo@gmail.com>
2019-03-10 10:16:52 +0200

    add index view

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ITOT
- * Date: 10/03/2019
- * Time: 09:10 AM
- */
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Moteur de recherche</title>
+</head>
+<body>
+    <form action="recherche.php" method="post">
+        <label for="recherche">Recherche :</label>
+        <input id="recherche" name="recherche" type="text" placeholder="Recherche..."/><br/>
+        <input type="submit" value="Rechercher">
+    </form>
+</body>
+</html>
