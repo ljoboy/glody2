edbccb3de8bc4fc048cc33f3ae1443c3d062c612
Dark Angel <jonathanyombo@gmail.com>
2019-03-10 09:37:51 +0200

    function recherche

--- models.php
+++ models.php
@@ -9,3 +9,17 @@ if (!function_exists('dbConnect')){
         return $db;
     }
 }
+
+if (!function_exists('recherche')){
+    function recherche($fichier){
+        $fichier = "%".$fichier."%";
+        $db = dbConnect();
+        $bdd = $db->prepare("SELECT * FROM fichiers WHERE nom LIKE :fichier");
+        $bdd->execute(['fichier'=>$fichier]);
+        $fichiers = [];
+        while ($f=$bdd->fetch(PDO::FETCH_ASSOC)){
+            $fichiers[] = $f;
+        }
+        return $fichiers;
+    }
+}
