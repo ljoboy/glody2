0494d65a3ed6f2f17e2aa1c30aad83a4d6db5160
Jonathan Yombo <jonathanyombo@gmail.com>
2020-01-12 12:54:55 +0200

    feat: recherche.php

--- recherche.php
+++ recherche.php
@@ -1,10 +1,10 @@
 <?php
-include_once 'models.php';
+require_once 'models.php';
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     if (!empty($_POST['recherche'])){
         $recherche = htmlspecialchars(strtolower($_POST['recherche']));
         $resultat = recherche($recherche);
-        include_once 'resultat.php';
+        require_once 'resultat.php';
     }else{
         header('location: index.php');
     }
